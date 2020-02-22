cc2ca02300bd3824e7bcdb81e826a89b6477830d
eonarmy <60788289+eonarmy@users.noreply.github.com>
2020-02-22 22:00:45 +0530

    New code

--- stdplugins/alive.py
+++ stdplugins/alive.py
@@ -1,16 +1,16 @@
 """Command To Check Bot is Alive or Not
-.alive"""
+.you_live"""
 import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("alive"))
+@borg.on(admin_cmd("you_live"))
 async def _(event):
     if event.fwd_from:
         return
-    mentions = "`Zinda Hu Sar (^^)/\n\nTelethon version: 1.9.0\nPython: 3.7.3\nUser: @arnab431\nDatabase Stats: Databases functioning normally!`"
+    mentions = "`Zinda Hu Sir \n\nUser Bot version: 1.9.0\nPython: 3.7.3\nBot Owner: @XHACKERKUNAL\nJON @EONARMY TO GET PREMIUM ACCOUNT LIKE NETFLIX,HOTSTAR,AMAZON PRIME AND ETC...!`"
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
         mentions += f""
