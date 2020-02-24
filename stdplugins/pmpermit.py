44d800496ee48e5a69d444faf7b5c580a6b8d50c
eonarmy <60788289+eonarmy@users.noreply.github.com>
2020-02-24 07:52:55 +0530

    Update pmpermit.py

--- stdplugins/pmpermit.py
+++ stdplugins/pmpermit.py
@@ -83,7 +83,7 @@ async def approve_p_m(event):
         if event.is_private:
             if is_approved(chat.id):
                 disapprove(chat.id)
-                await event.edit("Nikel BC MC BMC @XHACKERKUNAL BLOCK YOU..[{}](tg://user?id={})".format(firstname, chat.id)")
+                await event.edit("Nikel BC MC BMC @XHACKERKUNAL BLOCK YOU...")
                 await asyncio.sleep(3)
                 await borg(functions.contacts.BlockRequest(chat.id))
 
